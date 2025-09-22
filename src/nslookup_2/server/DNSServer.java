a7f01a90fd7da4742f17ac412114a74499d44880
Boima <Boimache27@gmail.com>
2025-09-22 21:06:42 +0700

    fix3

--- src/nslookup_2/server/DNSServer.java
+++ src/nslookup_2/server/DNSServer.java
@@ -6,17 +6,7 @@ import java.net.Socket;
 
 public class DNSServer {
     public static void main(String[] args) {
-        int port = 5050;
-        System.out.println("DNS Server running on port " + port);
 
-        try (ServerSocket serverSocket = new ServerSocket(port)) {
-            while (true) {
-                Socket clientSocket = serverSocket.accept();
-                new Thread(new ClientHandler(clientSocket)).start();
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
     }
 
     private static class ClientHandler implements Runnable {
@@ -28,23 +18,7 @@ public class DNSServer {
 
         @Override
         public void run() {
-            try (BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-                 PrintWriter out = new PrintWriter(socket.getOutputStream(), true)) {
 
-                String domain = in.readLine();
-                System.out.println("Received query: " + domain);
-
-                String result = DNSResolver.resolve(domain);
-
-                // gửi nhiều dòng
-                for (String line : result.split("\n")) {
-                    out.println(line);
-                }
-                out.println("END"); // đánh dấu kết thúc
-
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
         }
     }
 }
